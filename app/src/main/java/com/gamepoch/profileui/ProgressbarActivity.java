40bb34ddf194500529c0608d4d3551d47b5b742e
blade-demon <949659359@qq.com>
2018-05-27 12:46:30 +0800

    fix bugs

---
+++ app/src/main/java/com/gamepoch/profileui/ProgressbarActivity.java
@@ -0,0 +1,15 @@
+package com.gamepoch.profileui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ProgressbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progressbar);
+    }
+
+}
